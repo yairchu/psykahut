27bc3f4caf1529fcb367e4427c16884cb1ac4f02
Yair Chuchem <yairchu@gmail.com>
2018-07-29 20:11:23 +0300

    initial models

--- psykahut/admin.py
+++ psykahut/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+admin.site.register(Topic)
+admin.site.register(Question)
+admin.site.register(Game)
+admin.site.register(Answer)
+admin.site.register(Vote)
+admin.site.register(Participant)
--- psykahut/models.py
+++ psykahut/models.py
@@ -1,3 +1,34 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+# One topic for testing without revealing answers,
+# other used in actual game.
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+
+class Question(models.Model):
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    question_text = models.CharField(max_length=200)
+    # The real answer for the question
+    answer_text = models.CharField(max_length=200)
+
+class Game(models.Model):
+    started = models.DateTimeField('date created', auto_now_add=True)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    questions_asked = models.ManyToManyField(Question)
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    text = models.CharField(max_length=200)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    permutation_order = models.IntegerField()
+
+class Vote(models.Model):
+    player = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
+
+class Participant(models.Model):
+    player = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    score = models.IntegerField(default=0)
