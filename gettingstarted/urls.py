6afbb648f3d37dafcb7fd176de463f9a824a1d02
Yair Chuchem <yairchu@gmail.com>
2018-07-29 19:33:06 +0300

    index points to psykahut

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -4,14 +4,9 @@ from django.urls import path
 from django.contrib import admin
 admin.autodiscover()
 
-import hello.views
-
-# Examples:
-# url(r'^$', 'gettingstarted.views.home', name='home'),
-# url(r'^blog/', include('blog.urls')),
+import psykahut.views
 
 urlpatterns = [
-    url(r'^$', hello.views.index, name='index'),
-    url(r'^db', hello.views.db, name='db'),
+    url(r'^$', psykahut.views.index, name='index'),
     path('admin/', admin.site.urls),
 ]
